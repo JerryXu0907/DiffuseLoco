c14438c11821bd0e1e1160a8660700d11ece8205
-T.K.- <t_k_233@outlook.com>
2024-08-09 16:36:15 -0700

    FIX: fixing training script

--- scripts/train.py
+++ scripts/train.py
@@ -24,13 +24,13 @@ import pathlib
 
 from diffusion_policy.workspace.base_workspace import BaseWorkspace
 
+
 # allows arbitrary python code execution in configs using the ${eval:''} resolver
 OmegaConf.register_new_resolver("eval", eval, replace=True)
 
 @hydra.main(
     version_base=None,
-    config_path=str(pathlib.Path(__file__).parent.joinpath(
-        'diffusion_policy','config'))
+    config_path=str(pathlib.Path(__file__).parent.joinpath("../..", "diffusion_policy","config_files"))
 )
 def main(cfg: OmegaConf):
     # resolve immediately so all the ${now:} resolvers
